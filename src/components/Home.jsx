e1fe7c56dc2a9f7f83bf787604ed429ce01032a0
Omaan Mallick <omaanmallick786@gmail.com>
2024-07-27 23:57:13 +0530

    Initial Commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,32 +9,14 @@ function Home() {
         <div className="text-center">
        
     <Carousel>
-    <Carousel.Item>
-      <img
-        className="object-cover w-full h-[85vh] m-auto  bg-top shadow-[0_0_px_white]"
-        src={school2}
-        alt="First slide"/>
-      <Carousel.Caption>
-       <div className='flex-col'>
-          <h3 className="text-3xl mb-10 font-bold">Springdale Public School</h3> 
-          <p className='text-1xl mb-10'>
-            Welcome to Springdale Public School, where we nurture young minds for a brighter future. 
-          </p>
-        </div>
-      </Carousel.Caption>
-    </Carousel.Item>
+    
     <Carousel.Item>
       <img
         className="object-cover w-full h-[90vh] m-auto  bg-top shadow-[0_0_px_white]"
         src={school2}
         alt="First slide"/>
       <Carousel.Caption>
-       <div className='flex-col'>
-          <h3 className="text-3xl mb-10 font-bold">Springdale Public School</h3> 
-          <p className='text-1xl mb-10'>
-            Welcome to Springdale Public School, where we nurture young minds for a brighter future. 
-          </p>
-        </div>
+       
       </Carousel.Caption>
     </Carousel.Item>
 
@@ -44,12 +26,7 @@ function Home() {
         src={school2}
         alt="First slide"/>
       <Carousel.Caption>
-       <div className='flex-col'>
-          <h3 className="text-3xl mb-10 font-bold">Springdale Public School</h3> 
-          <p className='text-1xl mb-10'>
-            Welcome to Springdale Public School, where we nurture young minds for a brighter future. 
-          </p>
-        </div>
+     
       </Carousel.Caption>
     </Carousel.Item>
     
@@ -59,16 +36,11 @@ function Home() {
         src={school2}
         alt="First slide"/>
       <Carousel.Caption>
-       <div className='flex-col'>
-          <h3 className="text-3xl mb-10 font-bold">Springdale Public School</h3> 
-          <p className='text-1xl mb-10'>
-            Welcome to Springdale Public School, where we nurture young minds for a brighter future. 
-          </p>
-        </div>
+       
       </Carousel.Caption>
     </Carousel.Item>
    
-    3
+    
   </Carousel>
   </div>
       
