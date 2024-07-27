bb85be5f81c434535eb3ce16a8011c72f79c7a7b
Omaan Mallick <omaanmallick786@gmail.com>
2024-07-28 00:10:35 +0530

    Initial Commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,10 @@ import Academics from './components/Academics';
 import Home from './components/Home';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Students from './components/Students';
+import Admissions from './components/Admissions';
+import Faculty from './components/Faculty';
+import Gallery from './components/Gallery';
+import ContactUs from './components/ContactUs';
 
 function App() {
   return (
@@ -11,7 +15,11 @@ function App() {
     <Home/>
     <AboutUs/>
     <Academics />
+    <Admissions/>
+    <Faculty/>
     <Students/>
+    <Gallery/>
+    <ContactUs/>
     </>
   );
 }
